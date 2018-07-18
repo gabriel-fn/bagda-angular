6afbff95b7ea92dde8fba5cc39cd0bd955d6cfaa
Methir <methir.game@gmail.com>
2018-07-18 16:45:44 -0300

    Atualização rpg guards

--- src/app/rpg/guards/rpg.guard.ts
+++ src/app/rpg/guards/rpg.guard.ts
@@ -5,12 +5,13 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class RpgGuard implements CanActivate {
+  
   constructor(private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
     let id = route.params['id'];
     if (id && id > 0) {
-      return true
+      return true;
     } else {
       this.router.navigate(['rpgs']);
       return false;
