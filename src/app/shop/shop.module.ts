be0e8daa6e9a87262544fba043324d8528640275
Methir <methir.game@gmail.com>
2018-07-19 09:51:23 -0300

    Base para modulo shop

---
+++ src/app/shop/shop.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ShopRoutingModule } from './shop-routing.module';
+import { ShopComponent } from './shop/shop.component';
+import { ShopService } from './shop.service';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ShopRoutingModule
+  ],
+  declarations: [ShopComponent],
+  providers: [ShopService]
+})
+export class ShopModule { }
