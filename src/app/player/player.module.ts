f086a3bd4e671883beeaef1c30eca43af315fbdd
Methir <methir.game@gmail.com>
2018-08-23 14:03:50 -0300

    Eliminado os arquivos do player control component

--- src/app/player/player.module.ts
+++ src/app/player/player.module.ts
@@ -6,7 +6,6 @@ import { PlayerComponent } from './player/player.component';
 import { PlayerModalComponent } from './player/player-modal/player-modal.component';
 import { PlayerPainelComponent } from './player-painel/player-painel.component';
 import { PlayerService } from './player.service';
-//import { PlayerControlComponent } from './player-control/player-control.component';
 import { PlayerEditModalComponent } from './player-control/player-edit-button/player-edit-modal/player-edit-modal.component';
 import { SharedModule } from '../shared/shared.module';
 import { PlayerEditButtonComponent } from './player-control/player-edit-button/player-edit-button.component';
@@ -26,7 +25,6 @@ import { PlayerDeleteButtonComponent } from './player-control/player-delete-butt
     PlayerComponent,
     PlayerModalComponent,
     PlayerPainelComponent,
-    //PlayerControlComponent,
     PlayerEditModalComponent,
     PlayerEditButtonComponent,
     PlayerDeleteButtonComponent,
@@ -34,7 +32,6 @@ import { PlayerDeleteButtonComponent } from './player-control/player-delete-butt
   exports: [ 
     PlayerComponent,
     PlayerPainelComponent,
-    //PlayerControlComponent
   ],
   providers: [
     PlayerService,
