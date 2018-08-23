8969ba2a1c7bf6dc87a6be0219dc060f3e5b5615
Methir <methir.game@gmail.com>
2018-08-23 14:16:54 -0300

    Eliminada dependencias desnecessárias no modulo shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { Nl2BrPipeModule } from 'nl2br-pipe';
-import {  //MatTableModule, MatSortModule, MatPaginatorModule, 
-          MatButtonModule, MatInputModule, MatFormFieldModule, 
+import {  MatButtonModule, MatInputModule, MatFormFieldModule, 
           MatCardModule, MatSelectModule, MatExpansionModule, 
           MatListModule, MatDialogModule, MatProgressBarModule, 
           MatSidenavModule, MatToolbarModule, MatTabsModule, 
-          MatChipsModule, MatTooltipModule, MatSlideToggleModule, MatIconModule } from '@angular/material';
+          MatChipsModule, MatTooltipModule, MatSlideToggleModule, 
+          MatIconModule } from '@angular/material';
 
 import { ValidateInterceptor } from './interceptors/validate.interceptor';
 import { LoaderComponent } from './loader/loader.component';
@@ -19,9 +19,6 @@ import { ConfirmModalComponent } from './confirm-modal/confirm-modal.component';
   imports: [
     CommonModule,
     Nl2BrPipeModule,
-    //MatTableModule,
-    //MatSortModule,
-    //MatPaginatorModule,
     MatButtonModule,
     MatInputModule,
     MatFormFieldModule,
@@ -47,9 +44,6 @@ import { ConfirmModalComponent } from './confirm-modal/confirm-modal.component';
   exports: [
     LoaderComponent,
     Nl2BrPipeModule,
-    //MatTableModule,
-    //MatSortModule,
-    //MatPaginatorModule,
     MatButtonModule,
     MatInputModule,
     MatFormFieldModule,
